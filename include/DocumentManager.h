39e3ababb9ba5fdffc90e14c44c2e2589c3683a5
uneventful-paradise <davidtofan7@gmail.com>
2023-07-16 17:51:11 +0300

    finished find functionality

--- include/DocumentManager.h
+++ include/DocumentManager.h
@@ -3,6 +3,13 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
+#include <map>
+#include <algorithm>
+#include "Exceptions.h"
+
+#define NO_CHARS 256
+
 using namespace std;
 
 class DocumentManager {
@@ -10,12 +17,36 @@ class DocumentManager {
 	string filename;
 	vector<int>lineBuffer;
 	bool isModified;
-	int lenght;
+	int length;
+	string copyBuffer; 
 public:
+	//tools for handling files
     DocumentManager();
 	bool create(const string& filename);
 	bool open(const string& filename);
-	void modify();
 	bool save();
+	//tools for handling text
+	//7. charsInLine
+	//8. buffer position
+	
+	//TO-DO : save the document after each change (insertion /deletion)(via isChanged) or before closing
+	bool initializeLineBuffer();
+	int getLineCount();
+	bool insertText(int line, int col, const string& inserted);
+	bool insertLine(int line, const string& text);//overloaded function
+	bool deleteText(int line, int col, int size);//stack cu substringuri sterse
+	bool deleteLine(int line);
+	string getLine(int line);
+	bool swapLines(int line1, int line2);
+	vector<pair<string,int>>tokenize();//could also be a map?
+	vector<int> searchForWord(const string& word);//+pattern matching?
+	bool copy(int start, int end);
+	bool paste(int position);
+	
+	vector<int>badCharacterHeuristic(const string& pattern);
+	vector<int>failureF(const string& pattern);
+	vector<int>goodSuffixHeuristic(const string& pattern);
+	bool findUsingBM(const string& text, const string& pattern, int&i, int n, int m);
+	vector<int> find(const string& text, const string& pattern);
+	//void undo(vector<pair<bool (*fnc)(), vector<string>arguments>>)
 };
-
--- src/textEdd.cpp
+++ src/textEdd.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 using namespace std;
-#include "../include/DocumentManager.h"
+#include "DocumentManager.h"
 
 
 int main(int argc, char*argv[])
 {
-    string path;
+    string path; 
     char ch;
-    path = "../assets/testDoc.txt";
+    printf("Enter the document's path:\n");
+    getline(cin, path);
+    if (path == "") {
+        printf("no path in input, default path used\n-----------------------------------------------\n");
+        path = "D:/code/vs/varinate_sesiune/textEdd/textEdd/testDoc.txt";
+    }
+    printf("path of exe: %s\n-----------------------------------------------\n", argv[0]);
     DocumentManager document;
     document.open(path);
+    document.initializeLineBuffer();
+    document.insertText(20, 2, "da");
+    //document.deleteText(1, 1, 13);
+    //document.deleteLine(0);
+    //document.insertLine(0, "ababab");
+    //document.swapLines(1, 3);
+    //document.tokenize();
+    //document.searchForWord("linia");
+    //document.copy(0, 8);
+    //document.paste(24);
+
+    //document.insertText(2, 0, "abc\nd\n");
+    //document.copy(0, 7);
+    //document.paste(16);
+    //document.goodSuffixHeuristic("ABABACABA");
+    document.find("MAMAMAMA", "MAMA");
     return 0;
 }
